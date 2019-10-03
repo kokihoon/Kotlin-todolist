885cbc3b04c2428c545793689c6e26c6cfb981a4
kokihoon <rhrlgns777@gmail.com>
2019-10-03 14:16:22 +0900

    design UI

--- src/main/java/com/example/myapplication/MainActivity.kt
+++ src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.Presenter.ILoginPresenter
+import com.example.myapplication.Presenter.LoginPresenter
+import com.example.myapplication.View.ILoginView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),ILoginView {
+
+    override fun onLoginResult(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    internal lateinit var loginPresenter: ILoginPresenter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //init
+        loginPresenter = LoginPresenter(this);
+
+        //Event
+        btn_login.setOnClickListener {
+            loginPresenter.onLogin(edt_email.text.toString(), edt_password.text.toString())
+        }
     }
 }
